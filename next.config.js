442993bde3065e61023bf949fb17064815b2f131
Marko-sameh <markosameh222@hmail.com>
2026-02-14 15:42:47 +0200

    fix AI

--- next.config.js
+++ next.config.js
@@ -35,7 +35,7 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src 'self' fonts.gstatic.com; img-src 'self' data: https: blob:; connect-src 'self' https:; frame-ancestors 'none';",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src 'self' fonts.gstatic.com; img-src 'self' data: https: blob:; connect-src 'self' https://api-inference.huggingface.co; frame-ancestors 'none';",
           },
           {
             key: "X-Frame-Options",
